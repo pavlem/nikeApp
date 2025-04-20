637adaff46a2b8a8890afa84110a2d1e42ff7d3a
Pavle Mijatovic <paul.mijatovic@gmail.com>
2025-04-20 10:12:13 +0200

    Initial

---
+++ NikeT/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NikeT
+//
+//  Created by Pavle Mijatovic on 20. 4. 2025..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NikeT/NikeTApp.swift
@@ -0,0 +1,17 @@
+//
+//  NikeTApp.swift
+//  NikeT
+//
+//  Created by Pavle Mijatovic on 20. 4. 2025..
+//
+
+import SwiftUI
+
+@main
+struct NikeTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
